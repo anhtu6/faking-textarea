96c817a3227e441011db54492c42aeb304c4f26d
anhtu6 <anhtu632@gmail.com>
2020-02-26 11:45:30 +0700

    faking text area

---
+++ js/fake-textarea.js
@@ -0,0 +1,30 @@
+let string = "I am a supercool person!"
+let arrayText = string.split('');
+
+
+let s ='';
+let i =0;
+
+
+
+
+function inject() {
+        if(i==arrayText.length) {
+            i=0;
+            document.getElementById('content').value = "" ;
+            s ='';
+        } else {
+            s+=arrayText[i];
+
+
+            document.getElementById('content').value = s;
+            i++;
+            console.log(i)
+        }
+
+
+
+
+}
+
+
