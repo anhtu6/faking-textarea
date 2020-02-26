8f179a542b8564dc55582176696f1b490d863f84
anhtu6 <anhtu632@gmail.com>
2020-02-26 16:40:36 +0700

    updated

--- js/fake-textarea.js
+++ js/fake-textarea.js
@@ -1,30 +1,17 @@
 let string = "I am a supercool person!"
 let arrayText = string.split('');
-
-
-let s ='';
-let i =0;
-
-
-
-
+let s = '';
+let i = 0;
 function inject() {
-        if(i==arrayText.length) {
-            i=0;
-            document.getElementById('content').value = "" ;
-            s ='';
-        } else {
-            s+=arrayText[i];
-
-
-            document.getElementById('content').value = s;
-            i++;
-            console.log(i)
-        }
-
-
-
-
+    if (i == arrayText.length) {
+        i = 0;
+        document.getElementById('content').value = "";
+        s = '';
+    } else {
+        s += arrayText[i];
+        document.getElementById('content').value = s;
+        i++;
+    }
 }
 
 
